c3bada8594fc45fabd54847d69381ca444ed822b
AbubakerSuliman <40498532+AbubakerSuliman@users.noreply.github.com>
2020-05-12 12:36:25 +0400

    Update task.bat

--- task.bat
+++ task.bat
@@ -1,2 +1,2 @@
 @echo off
-R CMD BATCH C:\Users\Tyler\Desktop\Make_Task\task.R
+"C:\Program Files\R\R-3.6.2\bin\x64\R.exe" CMD BATCH "C:\Users\Abuabker Suliman\Downloads\Make_Task-master\Make_Task-master\task.R"
