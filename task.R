45a33f455b53495cfbffae8b820ababd0fac2484
AbubakerSuliman <40498532+AbubakerSuliman@users.noreply.github.com>
2020-05-12 12:27:52 +0400

    Update task.R

--- task.R
+++ task.R
@@ -1,22 +1,7 @@
-if (!"pacman" %in% dir(.libPaths())) devtools::install_github("trinker/pacman")
-pacman::p_load(rmarkdown, knitr)
-pacman::p_load_gh("trinker/gmailR")
+
+pacman::p_load(ggplot2)
 
 ## This will need to be changed to your local settings
-setwd("C:/Users/Tyler/Desktop/Make_Task")
+setwd("C:/Users/Abuabker Suliman/Downloads/Make_Task-master/Make_Task-master")
 
-unlink("reports", recursive = TRUE, force = FALSE)
-
-rmarkdown::render("apple_microsoft.Rmd", "all", output_dir = "reports")
-
-gmailR::gmail(
-    to="tyler_rinker@hotmail.com", 
-    subject = "Stock Reports", 
-    password = "PASSWORD",
-    message = "Please find the attached stock report.",
-    attachment = "reports/apple_microsoft.pdf",
-    username = "YOU_SOMEPLACE@gmail.com"
-)
-
-
-
+source("test_plot.R")
